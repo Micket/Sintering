4e7e2453c8f73d1dda58250028558f677e1d5201
Mikael Öhman <micketeer@gmail.com>
2010-09-17 19:25:29 +0200

    More things to keep track off

---
+++ matlab/RVE_generation/q1_to_q2.m
@@ -0,0 +1,149 @@
+%load coarse_mesh;
+%p = [[0,1,2,0,0.7,1.3,2,0.7,1.3,0,1,2]',[0,0,0,1,0.7,0.7,1,1.3,1.3,2,2,2]'];
+%t = [1,2,5; 2,6,5; 2,3,6; 3,7,6; 7,9,6; 7,12,9; 12,11,9; 11,8,9; 11,10,8; 10,4,8; 4,5,8; 4,1,5];
+%nodes_free = logical([0,0,0,0,1,1,0,1,1,0,0,0]);
+%[X,Y] = meshgrid(0:5,0:5); p = [X(:),Y(:)]; t = delaunay(p(:,1)',p(:,2)',{'Qt','Qz'});
+%p = [0,0; 1,0; 1,1; 0,1; 0.5, 0.5]; t = [4,1,5; 1,2,5; 2,3,5; 3,4,5];
+%nodes_free = logical(p(:,1)*0);
+
+load([fname,'.mat'])
+
+
+nodes_left   = p(:,1) == min(p(:,1));
+nodes_right  = p(:,1) == max(p(:,1));
+nodes_top    = p(:,2) == max(p(:,2));
+nodes_bottom = p(:,2) == min(p(:,2));
+
+% Construct all edges
+n = size(t,1);
+edges = sort([t(:,1),t(:,2); t(:,2),t(:,3); t(:,3),t(:,1)],2);
+et = [1:n,1:n,1:n]';
+[tmp,I] = sort(edges(:,1)*1e6 + edges(:,2));
+edges = edges(I,:);
+et = et(I);
+
+% Remove duplicates
+et = [et,zeros(size(et))];
+remove = zeros(size(edges,1),1);
+for i = 2:size(edges,1)
+    if edges(i,:) == edges(i-1,:)
+        remove(i) = true;
+        et(i-1,2) = et(i,1);
+    end
+end
+%remove(I) = remove;
+%edges(I,:) = edges;
+%et(I,:) = et;
+edges = edges(~remove,:);
+et = et(~remove,:);
+
+% Invert triangle->element list (et)
+te = zeros(n,3);
+k = zeros(n,1);
+for i = 1:size(et,1)
+    for j = 1:2
+        q = et(i,j);
+        if q > 0
+            k(q) = k(q) + 1;
+            te(q,k(q)) = i;
+        end
+    end
+end
+
+% Fix ordering... sigh, there must be some nicer way than all this shit.
+for i = 1:size(t,1)
+    ee = edges(te(i,:),:); % already sorted
+    tt1 = sort(t(i,[1,2]));
+    tt2 = sort(t(i,[2,3]));
+    tt3 = sort(t(i,[3,1]));
+    order = [find(ee(:,1) == tt1(1) & ee(:,2) == tt1(2)),...
+             find(ee(:,1) == tt2(1) & ee(:,2) == tt2(2)),...
+             find(ee(:,1) == tt3(1) & ee(:,2) == tt3(2))];
+    te(i,:) = te(i,order);
+end
+
+% Add nodes
+edges2 = [edges,(1:size(edges,1))'+size(p,1)];
+new_p = (p(edges(:,1),:) + p(edges(:,2),:))/2;
+
+% For the corner nodes (needed to find boundaries only)
+bc_nodes = nodes_left | nodes_right | nodes_top | nodes_bottom;
+
+% Construct new elements
+p2 = [p;new_p];
+t2 = [t,te+size(p,1)];
+
+nodes_left2   = p2(:,1) < min(p2(:,1))+1e-5;
+nodes_right2  = p2(:,1) > max(p2(:,1))-1e-5;
+nodes_bottom2 = p2(:,2) < min(p2(:,2))+1e-5;
+nodes_top2    = p2(:,2) > max(p2(:,2))-1e-5;
+
+bc_nodes2 = nodes_left2 | nodes_right2 | nodes_top2 | nodes_bottom2;
+
+% Invert edges
+ne = zeros(size(p,1),10);
+k = zeros(size(p,1),1);
+for i = 1:size(edges,1)
+    for j = 1:2
+        q = edges(i,j);
+        k(q) = k(q) + 1;
+        ne(q,k(q)) = i;
+    end
+end
+
+% Find edges
+x = ne(nodes_free,:); x = x(x>0);
+edges_free = find(sparse(x,ones(size(x)),ones(size(x)))>1);
+
+x = ne(nodes_left,:); x = x(x>0);
+edges_left = find(sparse(x,ones(size(x)),ones(size(x)))>1);
+
+x = ne(nodes_right,:); x = x(x>0);
+edges_right = find(sparse(x,ones(size(x)),ones(size(x)))>1);
+
+x = ne(nodes_top,:); x = x(x>0);
+edges_top = find(sparse(x,ones(size(x)),ones(size(x)))>1);
+
+x = ne(nodes_bottom,:); x = x(x>0);
+edges_bottom = find(sparse(x,ones(size(x)),ones(size(x)))>1);
+%
+
+% Plot those bastards
+hold on
+for i = 1:size(edges,1)
+    %plot(p(edges(i,:),1),p(edges(i,:),2),'-k.')
+    %plot(p2(edges2(i,[1,3,2]),1),p2(edges2(i,[1,3,2]),2),'-k.')
+end
+
+plot(new_p(:,1),new_p(:,2),'x')
+
+plot(p2(nodes_left2,1),p2(nodes_left2,2),'bo')
+plot(p2(nodes_right2,1),p2(nodes_right2,2),'bo')
+plot(p2(nodes_top2,1),p2(nodes_top2,2),'ro')
+plot(p2(nodes_bottom2,1),p2(nodes_bottom2,2),'ro')
+
+for i = 1:length(edges_free)
+    plot(p(edges(edges_free(i),:),1),p(edges(edges_free(i),:),2),'r');
+end
+for i = 1:length(edges_left)
+    plot(p(edges(edges_left(i),:),1),p(edges(edges_left(i),:),2),'b');
+end
+for i = 1:length(edges_right)
+    plot(p(edges(edges_right(i),:),1),p(edges(edges_right(i),:),2),'b');
+end
+for i = 1:length(edges_top)
+    plot(p(edges(edges_top(i),:),1),p(edges(edges_top(i),:),2),'r');
+end
+for i = 1:length(edges_bottom)
+    plot(p(edges(edges_bottom(i),:),1),p(edges(edges_bottom(i),:),2),'r');
+end
+
+%for i = 1:size(t2,1)
+%    px = p2(t2(i,[1,4,2,5,3,6,1]),1);
+%    py = p2(t2(i,[1,4,2,5,3,6,1]),2);
+%    plot(px,py)
+%end
+
+axis equal
+%axis ([-1.1,1.1,-1.1,1.1])
+
---
+++ matlab/RVE_generation/write_oofem_input.m
@@ -0,0 +1,101 @@
+clear all
+close all
+
+% 0.84, 0.88, 0.92, 0.95
+fname = '4particle_0.84_mesh';
+%fname = '4particle_0.88_mesh';
+%fname = '4particle_0.92_mesh';
+%fname = '4particle_0.95_mesh';
+%fname = '4particle_coarse_mesh';
+%fname = '4particle_super_coarse_mesh';
+q1_to_q2
+close all
+
+
+explanation = 'Simply 2D RVE with surface tension';
+gamma_s = 0.2;
+ndofman = size(p2,1);
+nelem = 0;
+r = 0.82; % radius of the embedded, stiff particle
+
+nodes = [];
+for i = 1:size(p2,1)
+    if i <= size(p,1)
+        dofidmask = [7,8,11];
+        BC = [0,0,0];
+    else
+        dofidmask = [7,8];
+        BC = [0,0];
+    end
+    %if nodes_left2(i);   BC(1) = 1; end
+    %if nodes_right2(i);  BC(2) = 2; end
+    %if nodes_top2(i);    BC(1) = 3; end
+    %if nodes_bottom2(i); BC(2) = 4; end
+    if bc_nodes2(i); BC(1:2) = 1; end
+    nodes = [nodes,...
+            sprintf('node %4d coords 2 %17.10e %17.10e ndofs %d DofIDMask %d %s bc %d %s\n',...
+            i,p2(i,:),size(dofidmask,2),size(dofidmask,2),num2str(dofidmask),size(BC,2),num2str(BC))];
+end
+
+q2q1trstokes = '';
+surfacetension = '';
+
+px = p(:,1); py = p(:,2);
+cx = mean(px(t),2); cy = mean(py(t),2);
+x1 = px(t(:,1)); x2 = px(t(:,2)); x3 = px(t(:,3));
+y1 = py(t(:,1)); y2 = py(t(:,2)); y3 = py(t(:,3));
+
+is_particle = sqrt((x1-1).^2+(y1-1).^2) < r | sqrt((x1+1).^2+(y1-1).^2) < r | sqrt((x1-1).^2+(y1+1).^2) < r | sqrt((x1+1).^2+(y1+1).^2) < r | ...
+              sqrt((x2-1).^2+(y2-1).^2) < r | sqrt((x2+1).^2+(y2-1).^2) < r | sqrt((x2-1).^2+(y2+1).^2) < r | sqrt((x2+1).^2+(y2+1).^2) < r | ...
+              sqrt((x3-1).^2+(y3-1).^2) < r | sqrt((x3+1).^2+(y3-1).^2) < r | sqrt((x3-1).^2+(y3+1).^2) < r | sqrt((x3+1).^2+(y3+1).^2) < r;
+mat = 1+is_particle;
+patch([x1,x2,x3]',[y1,y2,y3]',mat')
+%q2q1trstokes = sprintf('Q2Q1TrStokes %4d nodes 6 %3d %3d %3d %3d %3d %3d crossSect 1 mat 1\n',[(1:size(t2,1))'+nelem,t2]');
+for i = 1:size(t2,1)
+    q2q1trstokes = [q2q1trstokes,...
+        sprintf('Q2Q1TrStokes %4d nodes 6 %3d %3d %3d %3d %3d %3d crossSect 1 mat %d\n',i+nelem,t2(i,:),mat(i))];
+end
+nelem = nelem + size(t2,1);
+
+if size(edges_free,1) > 0
+    %surfacetension = sprintf('SurfaceTension2D %3d nodes 2 %3d %3d crossSect 1 mat 1 gamma_s %.5e\n',...
+    %    [(1:size(edges_free,1))'+nelem,edges(edges_free,:),gamma_s(ones(size(edges_free,1),1),1)]');
+    surfacetension = sprintf('SurfaceTension2DQ2 %3d nodes 3 %3d %3d %3d crossSect 1 mat 1 gamma_s %.5e\n',...
+        [(1:size(edges_free,1))'+nelem,edges2(edges_free,:),gamma_s(ones(size(edges_free,1),1),1)]');
+end
+nelem = nelem + size(edges_free,1);
+
+material = sprintf(['NewtonianFluid 1 d 1.0 mu 1.0\n'...
+                    'NewtonianFluid 2 d 1.0 mu 10.0\n']);
+
+grad = [0,0;0,0];
+dt = 0.05;
+rtolv = 1;
+
+boundarycondition = sprintf('PrescribedGradient 1 loadTimeFunction 1 ccord 2 0.0 0.0 gradient 2 2 {%e %e; %e %e}\n',grad');
+
+loadtimefunc = sprintf('ConstantFunction 1 f(t) 1.0\n');
+
+
+header = sprintf([...
+    '%s.out\n%s\n'...
+    'StokesFlow nsteps 1 lstype 3 smtype 7 nmodules 1 deltaT %f nodalupdatescheme 1 rtolv %e\n',...
+    'vtk tstep_all domain_all primvars 2 4 5 cellvars 1 46\n',...
+    'domain 2dIncompFlow\n',...
+    'OutputManager tstep_all dofman_all element_all\n',...
+    'ndofman %d nelem %d ncrosssect 1 nmat 2 nbc %d nic 0 nltf 1\n'],...
+    fname, explanation, dt, rtolv, ndofman, nelem, 1);
+ 
+fid = fopen([fname,'.in'],'w+');
+
+fwrite(fid,header);
+fwrite(fid,nodes);
+fwrite(fid,q2q1trstokes);
+fwrite(fid,surfacetension);
+fwrite(fid,sprintf('emptycs 1\n'));
+fwrite(fid,material);
+fwrite(fid,boundarycondition);
+fwrite(fid,loadtimefunc);
+
+system(['cp ',fname,'.in ~/workspace/OOFEM/']);
+
