b70122f238a2186eb4b83b211d60ef5427f8ff9b
Mikael Öhman <micketeer@gmail.com>
2012-05-01 22:38:54 +0200

    Updated bubble-example.

--- matlab/RVE_generation/write_bubble_wall.m
+++ matlab/RVE_generation/write_bubble_wall.m
@@ -1,15 +1,24 @@
 clear all
 
-m = 2;
+total_time = 25
+m = 3;
+e_size = 1/m;
+
+dt = 3*e_size;
+nsteps = ceil(total_time/dt)
+
+
 lpx = linspace(0,1,m);
+fprintf('Creating grid\n')
 [px,py] = meshgrid(lpx, lpx);
 p = [px(:), py(:)];
+fprintf('Triangulating\n')
 if m == 2
     t = [1,3,4;1,4,2];
 else
     t = delaunay(p(:,1), p(:,2));
 end
-
+disp('Sorting edges')
 n = size(t,1);
 edges = sort([t(:,1),t(:,2); t(:,2),t(:,3); t(:,3),t(:,1)],2);
 et = [1:n,1:n,1:n]';
@@ -18,6 +27,7 @@ edges = edges(I,:);
 et = et(I);
 
 % Remove duplicates
+disp('Removing duplicates')
 et = [et,zeros(size(et))];
 remove = zeros(size(edges,1),1);
 for i = 2:size(edges,1)
@@ -33,6 +43,7 @@ edges = edges(~remove,:);
 et = et(~remove,:);
 
 % Invert triangle->element list (et)
+disp('Inverting triangle->element list (et)')
 te = zeros(n,3);
 k = zeros(n,1);
 for i = 1:size(et,1)
@@ -46,6 +57,7 @@ for i = 1:size(et,1)
 end
 
 % Fix ordering... sigh, there must be some nicer way than all this shit.
+disp('Fixing ordering')
 for i = 1:size(t,1)
     ee = edges(te(i,:),:); % already sorted
     tt1 = sort(t(i,[1,2]));
@@ -58,22 +70,24 @@ for i = 1:size(t,1)
 end
 
 % Add nodes
+disp('Add nodes')
 new_p = (p(edges(:,1),:) + p(edges(:,2),:))/2;
 
 % Construct new elements
+disp('Construct new elements')
 p2 = [p;new_p];
 t2 = [t,te+size(p,1)];
 edges2 = [edges,(1:size(edges,1))'+size(p,1)];
 
-new_p_left   = new_p(:,1) < min(new_p(:,1))+1e-5;
-new_p_right  = new_p(:,1) > max(new_p(:,1))-1e-5;
-new_p_bottom = new_p(:,2) < min(new_p(:,2))+1e-5;
-new_p_top    = new_p(:,2) > max(new_p(:,2))-1e-5;
+new_p_left   = new_p(:,1) < min(new_p(:,1))+e_size/2;
+new_p_right  = new_p(:,1) > max(new_p(:,1))-e_size/2;
+new_p_bottom = new_p(:,2) < min(new_p(:,2))+e_size/2;
+new_p_top    = new_p(:,2) > max(new_p(:,2))-e_size/2;
 
-p2_left   = p2(:,1) < min(p2(:,1))+1e-5;
-p2_right  = p2(:,1) > max(p2(:,1))-1e-5;
-p2_bottom = p2(:,2) < min(p2(:,2))+1e-5;
-p2_top    = p2(:,2) > max(p2(:,2))-1e-5;
+p2_left   = p2(:,1) < min(p2(:,1))+e_size/2;
+p2_right  = p2(:,1) > max(p2(:,1))-e_size/2;
+p2_bottom = p2(:,2) < min(p2(:,2))+e_size/2;
+p2_top    = p2(:,2) > max(p2(:,2))-e_size/2;
 
 edges_left = find(new_p_left);
 edges_bottom = find(new_p_bottom);
@@ -82,6 +96,7 @@ edges_right = find(new_p_right);
 
 edges_free = find(new_p_right | new_p_top);
 
+disp('Writing input file')
 if 0
 clf
 hold on
@@ -103,11 +118,8 @@ end
 explanation = 'Simply 2D drop resting in a corner, driven by surface tension';
 fname = 'bubble_wall';
 gamma_s = [0.2,0.05,0.0];
-nsteps = 100;
-dt = 1;
 
 theta = acos((gamma_s(3) - gamma_s(2))/gamma_s(1))*180/pi
-
 
 ndofman = size(p2,1);
 nelem = 0;
@@ -128,9 +140,9 @@ for i = 1:size(p2,1)
         BC(2) = 1;
     end
 
-    nodes = [nodes,...
-            sprintf('node %4d coords 2 %17.10e %17.10e ndofs %d DofIDMask %d %s bc %d %s\n',...
-            i,p2(i,:),size(dofidmask,2),size(dofidmask,2),num2str(dofidmask),size(BC,2),num2str(BC))];
+    %nodes = [nodes,...
+    %        sprintf('node %4d coords 2 %17.10e %17.10e ndofs %d DofIDMask %d %s bc %d %s\n',...
+    %        i,p2(i,:),size(dofidmask,2),size(dofidmask,2),num2str(dofidmask),size(BC,2),num2str(BC))];
 end
 
 nelem = 0;
@@ -167,20 +179,41 @@ crosssections = sprintf('EmptyCS %d\n',1:5);
 
 header = sprintf([...
     '%s.out\n%s\n'...
-    'StokesFlow nsteps %d lstype 3 smtype 7 nmodules 1 deltaT %f nonlinform 2 rtolv %e\n',...
-    'vtk tstep_all domain_all primvars 2 4 5 cellvars 1 46\n',...
+    'StokesFlow nsteps %d lstype 3 smtype 7 nmodules 1 deltaT %f nonlinform 2 rtolv %e rtold 1000\n',...
+    'vtkxml tstep_all domain_all primvars 2 4 5 cellvars 1 46\n',...
     'domain 2dIncompFlow\n',...
     'OutputManager tstep_all dofman_all element_all\n',...
     'ndofman %d nelem %d ncrosssect 5 nmat 5 nbc 1 nic 0 nltf 1\n'],...
-    fname, explanation, nsteps, dt, 1, ndofman, nelem);
+    fname, explanation, nsteps, dt, 0.01, ndofman, nelem);
 
 fid = fopen([fname,'.in'],'w+');
 fwrite(fid,header);
-fwrite(fid,nodes);
+%fwrite(fid,nodes);
+for i = 1:size(p2,1)
+    if i <= size(p,1)
+        dofidmask = [7,8,11];
+        BC = [0,0,0];
+    else
+        dofidmask = [7,8];
+        BC = [0,0];
+    end
+    if p2_left(i) 
+        BC(1) = 1;
+    end
+    if p2_bottom(i)
+        BC(2) = 1;
+    end
+
+    fprintf(fid,'node %4d coords 2 %17.10e %17.10e ndofs %d DofIDMask %d %s bc %d %s\n',...
+            i,p2(i,:),size(dofidmask,2),size(dofidmask,2),num2str(dofidmask),size(BC,2),num2str(BC));
+end
+
 fwrite(fid,elements);
 fwrite(fid,crosssections);
 fwrite(fid,material);
 fwrite(fid,boundarycondition);
 fwrite(fid,loadtimefunc);
-system(['cp ',fname,'.in ~/workspace/OOFEM/']);
+
+fclose(fid);
+%system(['cp ',fname,'.in ~/workspace/OOFEM/']);
 
